501ee4f7673c11b1953c274f8950a4a9250ea98e
Trophoston <95759773+Trophoston@users.noreply.github.com>
2024-02-24 18:51:19 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -4,3 +4,13 @@
     window.addEventListener('load', function () {
         document.body.classList.add('loaded');
     });
+
+    const toastTrigger = document.getElementById('liveToastBtn')
+const toastLiveExample = document.getElementById('liveToast')
+
+if (toastTrigger) {
+  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+  toastTrigger.addEventListener('click', () => {
+    toastBootstrap.show()
+  })
+}
